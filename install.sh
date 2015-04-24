17ffa0e718590cdb13851f59c4088184d55b4e87
Kwadroke of "The Wired" <kwadroke@users.noreply.github.com>
2015-04-24 16:07:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 sudo echo framebuffer_width=640 >> /boot/config.txt
-sudo echo framebuffer_width=480 >> /boot/config.txt
+sudo echo framebuffer_height=480 >> /boot/config.txt
 sudo chown root:video /dev/fb*
 sudo usermod -a -G video pi
 cd /home/pi
-git clone http://github.com/kwadroke/space_pi-rates.git
+git clone http://github.com/kwadroke/rpispacepirates.git
 sudo apt-get update
 sudo apt-get install python-pip python-imaging
 sudo pip install pi3d
-sudo reboot
+echo Now reboot
